ef512ce6dcc793227eee56adec01f739cb564b22
Yohan Giarelli <yohan@un-zero-un.fr>
2018-11-16 08:03:32 +0100

    Changed env

--- docker/prod/deploy.sh
+++ docker/prod/deploy.sh
@@ -2,4 +2,4 @@
 
 DOCKER_IMAGE_APP=docker.un-zero-un.net/chat_app \
 DOCKER_IMAGE_NGINX=docker.un-zero-un.net/chat_nginx \
-docker stack deploy -c docker-compose.prod.yml otaa --with-registry-auth
+docker stack deploy -c docker-compose.prod.yml chat --with-registry-auth
