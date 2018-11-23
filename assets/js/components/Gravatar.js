5d1d9da3d924381890ed85539aac735ba0cacec8
Yohan Giarelli <yohan@un-zero-un.fr>
2018-11-23 09:45:56 +0100

    Fixed a trim on null

--- assets/js/components/Gravatar.js
+++ assets/js/components/Gravatar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import md5 from 'blueimp-md5';
 
 export default ({email}) => {
-    const hash = md5(email.trim());
+    const hash = md5((email || '').trim());
 
     return (
         <img src={`https://www.gravatar.com/avatar/${hash}`} />
--- assets/js/reducers/index.js
+++ assets/js/reducers/index.js
@@ -6,4 +6,6 @@ export default combineReducers({
     chat: combineReducers({
         messages,
     }),
+
+
 });
