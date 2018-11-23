6eb72fc70587736af82b7826af630cbb485284fe
Yohan Giarelli <yohan@un-zero-un.fr>
2018-11-23 09:47:16 +0100

    Added no avatar when no avatar

--- assets/js/components/Gravatar.js
+++ assets/js/components/Gravatar.js
@@ -2,7 +2,11 @@ import React from 'react';
 import md5 from 'blueimp-md5';
 
 export default ({email}) => {
-    const hash = md5((email || '').trim());
+    if (!email) {
+        return null;
+    }
+
+    const hash = md5(email.trim());
 
     return (
         <img src={`https://www.gravatar.com/avatar/${hash}`} />
